56a49a585ab6d0aae23bd95cc8d937b9bc44c7f3
luan-alencar <luanalencar134@gmail.com>
2021-09-29 06:26:58 -0300

    renomenado artifactID

--- gatewayservice/src/main/java/david/augusto/luan/gatewayservice/security/enumeration/Perfil.java
+++ gatewayservice/src/main/java/david/augusto/luan/gatewayservice/security/enumeration/Perfil.java
@@ -1,18 +1,21 @@
 package david.augusto.luan.gatewayservice.security.enumeration;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.Objects;
 
 @Getter
-@AllArgsConstructor
 public enum Perfil {
 
     ADMIN(1L, "ROLE_ADMIN"), CLIENTE(2L, "ROLE_CLIENTE");
 
     private Long id;
     private String descricao;
+
+    private Perfil(Long id, String descricao) {
+        this.id = id;
+        this.descricao = descricao;
+    }
 
     // Este método eu posso rodar mesmo sem ter o objeto instânciado
     public static Perfil toEnum(Long id) {
@@ -21,9 +24,9 @@ public enum Perfil {
             return null;
         }
 
-        for (Perfil e : Perfil.values()) {
-            if (id.equals(e.getId())) {
-                return e;
+        for (Perfil perfil : Perfil.values()) {
+            if (id.equals(perfil.getId())) {
+                return perfil;
             }
         }
         throw new IllegalArgumentException("ID inválido: " + id);
