997f697da61f651e3b4b025c29b2b1e0e13b40f4
luan-alencar <luanalencar134@gmail.com>
2021-09-30 10:16:07 -0300

    sistema e microservices rodando

--- accountservice/src/main/java/david/augusto/luan/accountservice/AccountserviceApplication.java
+++ accountservice/src/main/java/david/augusto/luan/accountservice/AccountserviceApplication.java
@@ -2,7 +2,9 @@ package david.augusto.luan.accountservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AccountserviceApplication {
 
--- inventoryservice/src/test/java/david/augusto/luan/inventoryservice/InventoryserviceApplicationTests.java
+++ inventoryservice/src/test/java/david/augusto/luan/inventoryservice/InventoryserviceApplicationTests.java
@@ -1,6 +1,6 @@
 package david.augusto.luan.inventoryservice;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
